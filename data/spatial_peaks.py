3ab914c3bd9751675f4fae7a1569ef0b14b76c86
OceanNuclear <annoyingamygdala@gmail.com>
2020-07-01 18:26:12 +0100

    Created python scripts for analysis

---
+++ data/spatial_peaks.py
@@ -0,0 +1,166 @@
+from numpy import array as ary; from numpy import log as ln
+from numpy import cos, sin, pi, sqrt, exp, arccos;
+tau = 2*pi
+import numpy as np;
+from matplotlib import pyplot as plt
+from scipy.ndimage import convolve
+import seaborn as sns
+from skimage.feature.blob import blob_dog, blob_doh, blob_log, peak_local_max
+from skimage.feature import hessian_matrix, hessian_matrix_det, hessian_matrix_eigvals
+from skimage.feature import local_binary_pattern
+from sklearn.metrics import confusion_matrix
+from scipy.ndimage import maximum_filter
+
+# Set path to data and filename. You can also store it a single variable
+# DATA_PATH = "../data/CeBr10k_1.txt" if you prefer. Here we expect to use at least the path itself later,
+# so we separate them.
+DATA_PATH = ""
+fname = "CeBr10k_1.txt"
+num_samples = 30
+# Lists to store each image, energy, position, and labels. We know the filesize, so we could create
+# arrays that perfectly match the data, but let's assume we don't know how many lines we're going to
+# read. 
+images, energies, positions = [], [], []
+# Open the file
+with open(DATA_PATH + fname, "r") as datafile:
+    # Loop over the file, line by line
+    for line in datafile:
+        
+        # The line is still a string when read from the file. We use numpys fromstring()
+        # to convert the line to a numpy array, specifying that each element is separated
+        # by a space. This does not convert the line in the file, only the "copy" that we have
+        # read into memory. fromstring() also removes any trailing newline ('\n') characters
+        # so we don't have to worry about that. The values will be interpreted as floats.
+        line = np.fromstring(line, sep=' ')
+        
+        # Now we pick slices of the array. The first 256 elements are 'pixels' of the detector image
+        image = line[:256]
+        
+        # Get the two energies, at index 256 and 259.
+        energy = np.array((line[256], line[259]))
+        
+        # And the four position values
+        pos = np.array((line[257], line[258], line[260], line[261]))
+
+        # Set label for the event. If Energy2 is 0 it is a single event. Any other values corresponds 
+        # to a double event. We label single events as type 0, and doubles as type 1. We could also 
+        # use Xpos2 or Ypos2 for this purpose.
+        if energy[1] == 0:
+            label = 0
+        else:
+            label = 1
+
+        # Finally, we take the separated arrays and add them to their respective "storage" lists.
+        images.append(image)
+        energies.append(energy)
+        positions.append(pos)
+
+    image = image.reshape([16,16])
+
+def is_local_max_2d(image):
+    """
+    Determine whether each pixel is a local maximum or not
+    Note that it will trim all edges,
+    i.e. if input has shape(n,m), it will return (n-1,m-1)
+    """
+    mright = image[1:-1, 1:-1]>=image[1:-1, 2:]
+    mleft = image[1:-1, 1:-1]>=image[1:-1, :-2]
+    mtop = image[1:-1, 1:-1]>=image[:-2, 1:-1]
+    mbottom = image[1:-1, 1:-1]>=image[2:,1:-1]
+
+    return np.logical_and(np.logical_and(mright, mleft), np.logical_and(mtop, mbottom))
+
+def diagonal_max_2d(image):
+    mtl = image[1:-1, 1:-1]>=image[:-2, :-2]
+    mtr = image[1:-1, 1:-1]>=image[:-2, 2:]
+    mbl = image[1:-1, 1:-1]>=image[2:, :-2]
+    mbr = image[1:-1, 1:-1]>=image[2:, 2:]
+    return np.logical_and(np.logical_and(mtl, mtr), np.logical_and(mbl, mbr))
+
+def make_wide(image):
+    wide_image = np.zeros(ary(image.shape)+2)
+    wide_image[1:-1, 1:-1] = image
+    wide_image[[0,-1],1:-1] = image[[0,-1],:]
+    wide_image[1:-1,[0,-1]] = image[:,[0,-1]]
+    return wide_image
+
+def is_local_max_2d_wide(image, diagonal=False):
+    """
+    does not trim the edges.
+    This is done by padding the edges with zeros.
+    """
+    wide_image = make_wide(image)
+    mask = is_local_max_2d(wide_image)
+    if diagonal:
+        mask = np.logical_and(diagonal_max_2d(wide_image), mask)
+    return mask
+
+# We've now looped over the entire file. The only thing that remains is to convert the lists
+# to numpy arrays.
+images = ary(images)
+energies = ary(energies)
+positions = ary(positions)
+
+if __name__=='__main__':
+    images = np.transpose(images.reshape([-1, 16, 16]), axes=[0,2,1])
+
+    laplacian = np.zeros([3,3])
+    laplacian[np.unravel_index(range(9)[1::2], (3,3))] = -1
+    laplacian[1,1] = 4
+    gaussian = np.ones([3,3])
+    gaussian[np.unravel_index(range(9)[1::2], (3,3))] = 2
+    gaussian[1,1] = 4
+    gaussian *= 1/16
+    prediction1, prediction2, prediction3 = [], [], []
+    for ind, image in enumerate(images):
+        # image_curvature = convolve(image, laplacian)
+        smooth_image = convolve(image, gaussian)
+        smooth_curvature = convolve(smooth_image, laplacian)
+        bulged_mask = smooth_curvature>smooth_curvature.max()*0.05
+        local_max_mask = is_local_max_2d_wide(image)
+
+        pred1 = np.logical_and(local_max_mask, bulged_mask).sum() # >90 % accuracy
+        prediction1.append(pred1)
+        # Unused (failed) methods
+        # pred1 = (image[is_local_max_2d_wide(image_curvature, diagonal=True)] > np.percentile(image.flatten(), 245/256*100)).sum() # 87.76 % accuracy
+        # pred2 = (image[is_local_max_2d_wide(smooth_image, diagonal=True)] > np.percentile(image.flatten(), 245/256*100)).sum() # 85.13 % accuracy
+        # # pred2 = (image[peak_local_max(image_curvature)]>np.percentile(image.flatten(), 250/256*100)).sum()
+        # pred3 = (image[peak_local_max(smooth_image)]>np.percentile(image.flatten(), 250/256*100)).sum()
+        # prediction2.append(pred2)
+        # prediction3.append(pred3)
+    labels = 2 - (energies==0).sum(axis=1)
+    # Peak must be at the center of a blob (contiguous area with 3+ pixels) where curvature is negative in both directions
+
+    print(confusion_matrix(labels, prediction1))
+
+    truth_pred = ary([labels, prediction1]).T
+
+    import sys
+    sys.exit()
+
+    chosen_to_be_examined = (truth_pred==[2,1]).all(axis=1)
+    for num, image in enumerate(images[chosen_to_be_examined][:num_samples]):
+        ind = np.where(chosen_to_be_examined)[0][num]
+        replay = True
+        while replay:
+            # introduce information about neighbourhood
+            image_curvature = convolve(image, laplacian)/image_curvature.max() # introduce some global information
+            smooth_image = convolve(image, gaussian)/smooth_image.max()
+            smooth_curvature = convolve(smooth_image, laplacian)/smooth_curvature.max()
+            bulged_mask = smooth_curvature>smooth_curvature.max()/8
+            local_max_mask = is_local_max_2d_wide(image)
+
+            for img, name in zip([image, image_curvature, smooth_image, smooth_curvature, bulged_mask, local_max_mask], ['image', 'image_curvature', 'smooth_image', 'smooth_curvature', 'bulged_mask', 'local_max_mask']):
+                sns.heatmap(img)
+                plt.title(f'{ind}: '+name+f' #events={labels[ind]}')
+                plt.show()
+            if len(input('Type any character before presssing enter to move onto the next plot'))>0:
+                replay=False
+
+# OK let's do reverse learning:
+# First, lets examine the characteristics of the peaks vs non-peaks:
+    curvature = ary([convolve(image, laplacian) for image in images])
+    smoothened= ary([convolve(image, gaussian) for image in images])
+    smooth_curve = ary([convolve(smooth_image, laplacian) for smooth_image in smoothened])
+    local_max = ary([is_local_max_2d_wide(image, diagonal=True) for image in images])
+    
---
+++ data/temporal_peaks_convolve.py
@@ -0,0 +1,210 @@
+from numpy import array as ary; from numpy import log as ln
+from numpy import cos, sin, pi, sqrt, exp, arccos;
+tau = 2*pi
+import numpy as np;
+from matplotlib import pyplot as plt
+import pandas as pd
+import seaborn as sns
+from scipy.signal import find_peaks, find_peaks_cwt
+from sklearn.metrics import confusion_matrix
+
+# rise_min, rise_max = 0.85, 1.15
+rise_min, rise_max = 0.75, 1.2
+# dec_min, dec_max = 1/0.065, 1/0.055
+dec_min, dec_max = 10, 30
+
+rise_resolution, dec_resolution = 100, 100
+# average values to be used
+rise_time, decay_time = 1.025, 16
+
+norm_plot = lambda x: plt.plot(x/abs(x).max())
+
+def get_basis(rise_time, decay_time, length=200, offset=0):
+    """
+    Generate the basis vector for a given rise time and decay time.
+    for the simulated dataset training_pm_nosat_150k.dat,
+    rise_time = (0.85, 1.15]
+    decay_time=(15.38463905329085, 18.18]
+    """
+    t = np.arange(-length+offset, length+offset)
+    basis = exp(t *(1/rise_time-1/decay_time))/(1+exp(t/rise_time)) 
+    return basis/basis.sum()
+
+def get_diff_basis(rise_time, decay_time, length=200, offset=0):
+    """
+    Get the derivative of the basis, normalized by its rms area.
+    rms_area seems to be the only way to ensure that it works
+    """
+    t = np.arange(-length+1, length)
+    diff_basis = 1/rise_time *exp((-1/rise_time-1/decay_time)*t)/(1+exp(-t/rise_time))**2
+    diff_basis -= 1/decay_time * exp(-t/decay_time)/(1+exp(-t/rise_time))
+
+    #These other four normalization scalars are useless
+    # first_moment = abs(t @ diff_basis )
+    # second_moment = abs(t**2 @ diff_basis)
+    # area = abs(diff_basis).sum()
+    # max_point = abs(diff_basis).max()
+    rms_area = sqrt((diff_basis**2).mean())
+    return diff_basis/( rms_area**0.91 ) # after some experimentation, the exponent of 0.91 seems to be the best at minimizing the stuck_point_count.
+    # Other normalization scalars usable include: decay_time**n, and rise_time**n.
+
+def get_smooth_diff_conv_matrix(rise_time, decay_time, resolution=500, length=200):
+    """
+    Get the smoothened version of the convolution matrix
+    """
+    matrix = []
+    for start_point in np.linspace(0, -length, resolution):
+        decimal_offset = start_point%1
+        int_offset = int(start_point//1)
+        basis = get_basis(rise_time, decay_time, length=length, offset=decimal_offset)[length+int_offset:2*length+int_offset]
+        matrix.append(np.diff(basis))
+    return ary(matrix)
+
+def get_smooth_conv_matrix(rise_time, decay_time, resolution=500, length=200):
+    """
+    Get the smoothened version of the convolution matrix
+    """    
+    matrix = []
+    for start_point in np.linspace(0, -length, resolution):
+        decimal_offset = start_point%1
+        int_offset = int(start_point//1)
+        basis = get_basis(rise_time, decay_time, length=length, offset=decimal_offset)[length+int_offset:2*length+int_offset]
+        matrix.append(basis)
+    return ary(matrix)
+
+def get_curve_basis(rise_time, decay_time, length=200):
+    """
+    get the second order derivative of the basis
+    """ 
+    t = np.arange(-length+1, length)
+    diff_basis = 1/rise_time *exp((-1/rise_time-1/decay_time)*t)/(1+exp(-t/rise_time))**2
+    diff_basis -= 1/decay_time * exp(-t/decay_time)/(1+exp(-t/rise_time))
+    return np.diff(diff_basis)
+
+def get_diff_convolve_matrix(trace_length=200):
+    """
+    Get the convolution matrix where matrix @ trace = strength of that basis
+    """
+    vlong_matrix = []
+    for rise in np.linspace(rise_min, rise_max, rise_resolution):
+        long_matrix = []
+        for dec in np.linspace(dec_min, dec_max, dec_resolution):
+            long_matrix.append(get_diff_basis(rise, dec, length=trace_length))
+        vlong_matrix.append(long_matrix)
+    vlong_matrix = ary(vlong_matrix, dtype=float)
+    total_length = vlong_matrix.shape[-1]
+    return ary([vlong_matrix[:,:, trace_length-i:total_length-i] for i in range(trace_length)], dtype=float)
+
+def get_curve_convolve_matrix(trace_length=200):
+    """
+    Get the convolution matrix where matrix @ trace = strength of that basis
+    (This matrix uses the second derivative (i.e. curvature) of the basis)
+    """
+    vlong_matrix = []
+    for rise in np.linspace(rise_min, rise_max, rise_resolution):
+        long_matrix = []
+        for dec in np.linspace(dec_min, dec_max, dec_resolution):
+            long_matrix.append(get_curve_basis(rise, dec, length=trace_length))
+        vlong_matrix.append(long_matrix)
+    vlong_matrix = ary(vlong_matrix, dtype=float)
+    total_length = vlong_matrix.shape[-1]
+    return ary([vlong_matrix[:,:, trace_length-i:total_length-i] for i in range(trace_length)], dtype=float)
+
+def is_local_max_3d(image):
+    """
+    Determine if pixel is a local maximum or not.
+    Note that it returns an array with trimmed edges,
+    i.e. if input has shape (n,m,k), it will return (n-1, m-1, k-1)
+    """
+    mright = image[1:-1, 1:-1, 1:-1]>=image[1:-1, 1:-1, 2:]
+    mleft = image[1:-1, 1:-1, 1:-1]>=image[1:-1, 1:-1, :-2]
+    mtop = image[1:-1, 1:-1, 1:-1]>=image[1:-1, :-2, 1:-1]
+    mbottom = image[1:-1, 1:-1, 1:-1]>=image[1:-1, 2:,1:-1]
+    mfront = image[1:-1, 1:-1, 1:-1]>=image[:-2, 1:-1, 1:-1]
+    mback = image[1:-1, 1:-1, 1:-1]>=image[2:, 1:-1, 1:-1]
+
+    return np.logical_and(
+        np.logical_and(np.logical_and(mright, mleft), np.logical_and(mtop, mbottom)),
+        np.logical_and(mfront, mback)
+    )
+
+def is_local_max_1d(series):
+    mleft = series[1:-1]>=series[:-2]
+    mright = series[1:-1]>=series[2:]
+    return np.logical_and(mleft, mright)
+
+def get_ricker_matrix(length, widths):
+    """
+    get the ricker wavelet convolution matrix
+    provided a list of widths
+    """
+    vary_width = ary([ricker(length, w) for w in widths])
+    return ary([np.roll(vary_width, i) for i in range(length)])
+
+if __name__=='__main__':
+    df = pd.read_csv('training_pm_nosat_150k.dat', sep=' ', header=None)
+    # df[1] is just an integer 200. I don't know why.
+    # Below are Information which won't be available in the experimental data: (i.e. labels usable for training)
+    num_events = df[0]
+    amp1, amp2 = df[2], df[7]
+    rise1, rise2 = df[3], df[8]                 # default decay2=0
+    decay1, decay2 = df[4], df[9]               # default pos2=0
+    offset, pos1, pos2 = df[6], df[5], df[10]   # default pos2=0
+    # Information which will actually be avialble in the experiment
+    wave_forms = df[df.columns[11:]]
+    print('Data Read. Extracting useful information out of the data...')
+    wave_forms.columns = range(wave_forms.shape[1])
+    print("Renamed columns, shifting upwards...")
+
+    time_derivative = np.diff(wave_forms.values, axis=1)
+    # curvature = np.diff(time_derivative, axis=1)
+    
+    # long_matrix = get_diff_convolve_matrix()
+    conv_matrix = get_smooth_diff_conv_matrix((rise_min + rise_max)/2, (dec_min+dec_max)/2, 600)
+    peak_findable = (conv_matrix @ time_derivative.T).T
+
+    labels=num_events.values
+    prediction = []
+    for line_of_peaks in peak_findable:
+        peak_loc = find_peaks(line_of_peaks, height=0.5) # tune height according to line_of_peaks.max()?
+        prediction.append(line_of_peaks[peak_loc[0]].__len__())
+    prediction = ary(prediction)
+
+    print(confusion_matrix(labels, prediction))
+
+    truth_pred = ary([labels, prediction]).T
+
+    diff_peaks = np.diff(peak_findable, axis=1) # this gives extra information
+    # examine the incorrectly predicted data:
+    for num, ind in enumerate(np.arange(len(peak_findable))[(truth_pred==[2,1]).all(axis=1)]):
+        norm_plot(peak_findable[ind])
+        norm_plot(wave_forms.loc[ind])
+        plt.title(f'{ind=}, amp={amp1[ind]}, {amp2[ind]}')
+        plt.show()
+        if num>20:
+            break
+
+    # stuck_point_count = 0
+    # for idx, line in enumerate(time_derivative):
+    #     argmax = np.argmax(long_matrix @ line)
+    #     ind = np.unravel_index(argmax, long_matrix.shape[:-1])
+    #     if ind[0]!=np.clip(ind[0], 1, long_matrix.shape[0]-2):
+    #         stuck_point_count+=1
+    #     if ind[1]!=np.clip(ind[1], 1, long_matrix.shape[1]-2):
+    #         stuck_point_count+=1
+    #     if ind[2]!=np.clip(ind[2], 1, long_matrix.shape[2]-2):
+    #         stuck_point_count+=1
+    #     print(idx, ind, stuck_point_count/np.clip(idx,1,None))
+
+    # for i in range(112, 125):
+    #     sns.heatmap(long_matrix[i]@time_derivative[0])
+    #     plt.title(f'ind={i}')
+    #     plt.show()
+    if False: # useless methods
+        for ind, line in enumerate(time_derivative):
+            mask = is_local_max_3d(long_matrix@line)
+            print(f'{ind=}, {mask.sum()=}')
+        laplacian3d = np.zeros([3,3,3])
+        laplacian3d[np.unravel_index(range(27)[::2], (3,3,3))] = -1
+        laplacian3d[::2,::2,::2] = 0
+        laplacian3d[1,1,1] = 6
